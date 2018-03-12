de4c2cbc7031040345e854f5f2d6a108ff88e91f
Renan S Moreira <renan.siravegna@gmail.com>
2018-03-12 10:25:26 -0400

    Usando o novo serviço de ocr no worker

--- worker/src/index.js
+++ worker/src/index.js
@@ -5,6 +5,7 @@ const amqp = require('amqplib/callback_api');
 const tesseract = require('node-tesseract');
 const uuid = require('uuid/v4');
 const marky = require('marky');
+const ocr = require('./ocr');
 
 const fila_de_entrada = "para_processamento";
 const fila_de_processados = "processados";
@@ -33,7 +34,7 @@ function enviar_para_fila_de_nao_processados(canal, mensagem) {
   canal.assertQueue(fila_de_nao_processados, {durable: true});
   canal.sendToQueue(fila_de_nao_processados, new Buffer(mensagem), {persistent : true});
 }
- 
+
 function processarDocumento(ch, mensagemDaFila) {
     try {
       esta_processando = true;
@@ -42,44 +43,33 @@ function processarDocumento(ch, mensagemDaFila) {
       console.info(' [x] Baixando %s', mensagem.url);
 
       const pathDaImagemBaixada = `./${uuid()}.jpg`;
-      
+
       marky.mark('download');
       download(mensagem.url, pathDaImagemBaixada, () => {
-        const tempo_de_download = marky.stop('download');
-        console.info(' [x] Download finalizado: %s ms', tempo_de_download.duration);
+        try {
+          const textoDaImagem = await ocr(pathDaImagemBaixada);
+          const resposta = JSON.stringify({ texto: textoDaImagem, id: mensagem.id });
 
-        marky.mark('ocr');
-        tesseract.process(pathDaImagemBaixada, tesseract_options, function(erro, text) {
-          const tempo_de_ocr = marky.stop('ocr');
-          console.info(' [x] OCR finalizada: %s ms', tempo_de_ocr.duration);
+          fs.unlink(pathDaImagemBaixada, (erro) => {
+            if (erro)
+              return;
 
-          if (erro) {
-            console.error(erro);
-            ch.nack(mensagemDaFila);
+            enviar_para_fila_de_processados(ch, resposta);
 
-            throw new Error(erro.message);
-          }
-          
-          else {
-            const resposta = {texto: text, id: mensagem.id }
-            const respostaEmJson = JSON.stringify(resposta);
-            
-            fs.unlink(pathDaImagemBaixada, (erro) => {
-              if (erro)
-                return;
-
-              enviar_para_fila_de_processados(ch, respostaEmJson);
-
-              ch.ack(mensagemDaFila);
-              esta_processando = false;
-            });
-          }
-        });
+            ch.ack(mensagemDaFila);
+            esta_processando = false;
+          });
+        }
+        catch (erroDeLeituraDoOcr) {
+          console.error(erro);
+          ch.nack(mensagemDaFila);
+          throw new Error(erro.message);
+        }
       });
     }
     catch(excecao) {
       console.error(excecao);
-      
+
       enviar_para_fila_de_nao_processados(ch, mensagemDaFila.content.toString());
 
       ch.ack(mensagemDaFila);
