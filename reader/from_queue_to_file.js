7bfb33b60cfba08f7af0b82443165a049b3aa728
Renan S Moreira <renan.siravegna@gmail.com>
2018-03-14 17:39:00 -0400

    Ajustando o gerador de arquivos em disco para que ele crie instruções sql prontas para serem executadas

--- reader/from_queue_to_file.js
+++ reader/from_queue_to_file.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const argv = require('yargs').argv
 const amqp = require('amqplib/callback_api');
-const queue_name = "processados";
+const queue_name = argv.queue;
+
+const _root_dir = './scripts';
+
+create_root_dir();
 
 amqp.connect(argv.amqp_uri, (error, connection) => {
   connection.createChannel((error, channel) => {
@@ -10,16 +14,27 @@ amqp.connect(argv.amqp_uri, (error, connection) => {
     channel.consume(queue_name, (message) => save_to_file(channel, message), {noAck: false});
   });
 });
- 
+
+function create_root_dir() {
+  if (fs.existsSync(_root_dir))
+    return;
+
+  fs.mkdirSync(_root_dir);
+}
+
 function save_to_file(channel, message) {
   const document = JSON.parse(message.content.toString());
-  
-  fs.writeFile(`./files/${document.id}.txt`, document.texto, (error) => {
+  const sql = `UPDATE imagens SET resultadoocr = '${document.texto.replace('\'', '\\\'')}' WHERE imagem_cd = ${document.id}`;
+
+  fs.writeFile(`./scripts/${document.id}.sql`, sql, { encoding: 'utf8' }, (error) => {
     if (error) {
+      console.log(' [x] Erro');
       channel.nack(message);
       return;
     }
 
-    channel.ack(message);
+    console.log(` [x] Gerado ${document.id}`);
+
+    channel.nack(message);
   });
 }
